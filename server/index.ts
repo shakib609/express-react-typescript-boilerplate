4c83b93b0a029a83ec31fe369df0cf84f225594b
Shakib Hossain <shakib609@gmail.com>
2019-07-07 13:04:09 +0600

    Add morgan logger middleware

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,12 @@
 import express, { Application } from 'express'
+import morgan from 'morgan'
 import path from 'path'
 
 const app: Application = express()
+
+// Enable morgan logger
+const morganConfig = process.env.NODE_ENV === 'production' ? 'short' : 'dev'
+app.use(morgan(morganConfig))
 
 // Serve static files from React apps production build
 const staticFilesPath = path.resolve(__dirname, '../client/build/')
